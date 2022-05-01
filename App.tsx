1c6d8144970efb09f71096395575e95c675cfe5c
Michael Craun <michael.craun@gmail.com>
2022-04-30 21:04:42 -0400

    Added navigation and tab capabilities; set up initial tab screens

--- App.tsx
+++ App.tsx
@@ -1,12 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet, Text, View } from 'react-native';
+import { DataController } from './screens/data';
+import { ExpensesController } from './screens/expenses';
+import { IncomeController } from './screens/income';
+import { MaterialIcons } from '@expo/vector-icons';
+
+const Tabs = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+<NavigationContainer>
+  <Tabs.Navigator screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconColor = focused ? "black" : "gray"
+
+        switch (route.name) {
+          case "income": return <MaterialIcons name="attach-money" size={24} color={iconColor} />
+          case "expenses": return <MaterialIcons name="money-off" size={24} color={iconColor} />
+          case "data": return <MaterialIcons name="bar-chart" size={24} color={iconColor} />
+          default: return <MaterialIcons name="attach-money" size={24} color={iconColor} />
+        }
+      },
+      tabBarActiveTintColor: 'tomato',
+      tabBarInactiveTintColor: 'gray',
+    })}>
+    <Tabs.Screen name="income" component={IncomeController} options={{title:"Income"}} />
+    <Tabs.Screen name="expenses" component={ExpensesController} options={{title:"Expenses"}} />
+    <Tabs.Screen name="data" component={DataController} options={{title:"Data"}} />
+  </Tabs.Navigator>
+</NavigationContainer>
   );
 }
 
---
+++ screens/income.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export const IncomeController = () => {
+    return <View></View>
+}
